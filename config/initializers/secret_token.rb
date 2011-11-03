d2e3b4803df332a5a0ffe3f42397bf3fb2055462
Rafael Fernández López <ereslibre@ereslibre.es>
2011-11-03 20:07:04 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Botich::Application.config.secret_token = '0357a59446bedb0b477ee3edf3d9cd2ecbda3b61afc965d9b4365dac1be15f171da26cdfb6a530ee5e6b049a50eaaaaaa92b91678da5120c27a10ea3255e965b'
